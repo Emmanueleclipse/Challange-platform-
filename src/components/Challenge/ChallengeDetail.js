3a1ec87195b6ec2a8fa071d346dfa7e49339ee98
TalentedSunday <sunasunday111@gmail.com>
2023-08-03 19:58:20 +0500

    some ui fix for challenge detail

--- src/components/Challenge/ChallengeDetail.js
+++ src/components/Challenge/ChallengeDetail.js
@@ -74,6 +74,8 @@ export default function ChallengeDetail() {
 
   const [results, setResults] = useState([])
 
+  const [sending, setSending] = useState(false)
+
   const [loadingFunds, setLoadingFunds] = useState(false)
 
   useFetchUser()
@@ -187,6 +189,7 @@ export default function ChallengeDetail() {
 
   const handleAgree = useCallback(() => {
     setLoading(true)
+    setSending(true)
     jwtDecode(localStorage.getItem('token')).then((res) => {
       const pKey = res?.private_key
       decryptMnemonic(pKey, secretKey).then((res) => {
@@ -222,6 +225,8 @@ export default function ChallengeDetail() {
               {
                 onSuccess: () => {
                   setLoading(false)
+                  setSending(false)
+
                   reload()
                 },
                 onError: () => {
@@ -298,6 +303,7 @@ export default function ChallengeDetail() {
 
   const handlePayouts = useCallback(() => {
     setLoading(true)
+    setSending(true)
     toast.success(
       'Please wait while the funds are being transferring to the winners of this challenge. It takes upto 2-3 minutes approx',
       {
@@ -323,6 +329,7 @@ export default function ChallengeDetail() {
             setResults(results)
           }
           setLoadingFunds(false)
+          setSending(false)
         },
         onError: () => {
           toast.error(`Unexceptional Error`)
@@ -336,7 +343,7 @@ export default function ChallengeDetail() {
       <Container maxWidth='md' className={classes.container}>
         <PageWrapper display='flex' flexDirection='row'>
           <Box display='flex' alignItems='justify-between' flexDirection='row'>
-            <Box display='flex' alignItems='center' flexDirection='column'>
+            <Box display='flex' alignItems='start' flexDirection='column'>
               <Typography variant='h3'>
                 <strong>Steps</strong>
               </Typography>
@@ -351,7 +358,7 @@ export default function ChallengeDetail() {
                     <Form autoComplete='off'>
                       <Grid
                         container
-                        justifyContent='center'
+                        justifyContent='start'
                         display='flex'
                         spacing={2}
                       >
@@ -386,7 +393,6 @@ export default function ChallengeDetail() {
                             variant='contained'
                             color='primary'
                             type='submit'
-                            fullWidth
                             size='large'
                             disabled={false}
                           >
@@ -400,108 +406,103 @@ export default function ChallengeDetail() {
               </Formik>
             </Box>
             <Box display='flex' alignItems='center' flexDirection='column'>
-              {loading ? (
-                <CircularProgress />
-              ) : (
-                <>
-                  <Typography variant='h3'>
-                    <strong>{challenge?.name}</strong>
+              <>
+                <Typography variant='h3'>
+                  <strong>{challenge?.name}</strong>
+                </Typography>
+                <Box mb={3} />
+                {status === STATUS_UPCOMING && (
+                  <Chip label='UPCOMING' color='secondary' />
+                )}
+                {status === STATUS_STARTED &&
+                  !challenge?.fund_delivered_flag && (
+                    <Chip label='ACTIVE' color='primary' />
+                  )}
+                {status === STATUS_ENDED && challenge?.fund_delivered_flag && (
+                  <Chip label='Already Ended.' />
+                )}
+                {!challenge?.fund_delivered_flag && (
+                  <Typography variant='h5'>
+                    {`${liveTime.days} Day${liveTime.days > 1 ? 's' : ''} ${
+                      liveTime.hours
+                    } h ${liveTime.minutes} min ${liveTime.seconds} s`}
                   </Typography>
-                  <Box mb={3} />
-                  {status === STATUS_UPCOMING && (
-                    <Chip label='UPCOMING' color='secondary' />
-                  )}
-                  {status === STATUS_STARTED &&
-                    !challenge?.fund_delivered_flag && (
-                      <Chip label='ACTIVE' color='primary' />
-                    )}
-                  {status === STATUS_ENDED &&
-                    challenge?.fund_delivered_flag && (
-                      <Chip label='Already Ended.' />
-                    )}
-                  {!challenge?.fund_delivered_flag && (
-                    <Typography variant='h5'>
-                      {`${liveTime.days} Day${liveTime.days > 1 ? 's' : ''} ${
-                        liveTime.hours
-                      } h ${liveTime.minutes} min ${liveTime.seconds} s`}
-                    </Typography>
-                  )}
+                )}
 
-                  <Box mt={4}>
-                    <Typography variant='h6'>
-                      Total Participants:&nbsp;
-                      {challenge?.participant
-                        ? challenge?.participant.length
-                        : 0}
-                    </Typography>
-                  </Box>
-                  <Box mt={4}>
-                    <Typography variant='h6'>
-                      Coordinator:&nbsp;
-                      <Link href={`/profile/${challenge?.coordinator}`}>
-                        {challenge?.coordinator}
-                      </Link>
-                    </Typography>
-                  </Box>
-                  {status === STATUS_ENDED && <>Success Rate: 0</>}
-                  <Box mt={4} width={400} maxWidth='100%'>
-                    <Formik
-                      initialValues={{ betAmount }}
-                      onSubmit={handleAgree}
-                      validationSchema={validationSchema}
-                      enableReinitialize
-                    >
-                      {({ values, touched, errors, handleChange }) => {
-                        const handleChangeAndSetBetAmount = (e) => {
-                          handleChange(e)
-                          setBetAmount(e.target.value)
-                        }
-                        return (
-                          <Form autoComplete='off'>
-                            <Grid
-                              container
-                              justifyContent='center'
-                              display='flex'
-                              spacing={2}
-                            >
-                              {status !== STATUS_ENDED &&
-                              localStorage.getItem('identity') !==
-                                challenge?.coordinator &&
-                              !challenge?.fund_delivered_flag ? (
-                                <>
-                                  <Grid item xs={12} sm={9}>
-                                    <Field name='minBet' type='number'>
-                                      {({ field }) => (
-                                        <div>
-                                          <TextField
-                                            {...field}
-                                            type='number'
-                                            label='Add Bet'
-                                            variant='outlined'
-                                            onChange={
-                                              handleChangeAndSetBetAmount
-                                            }
-                                            value={betAmount}
-                                            error={
-                                              touched.minBet &&
-                                              Boolean(errors.minBet)
-                                            }
-                                            helperText={
-                                              touched.minBet && errors.minBet
-                                            }
-                                          />
-                                          {values.betAmount <
-                                            challenge?.add_bet && (
-                                            <p style={{ color: 'red' }}>
-                                              {`Minimum bet amount must be greater than
+                <Box mt={4}>
+                  <Typography variant='h6'>
+                    Total Participants:&nbsp;
+                    {challenge?.participant ? challenge?.participant.length : 0}
+                  </Typography>
+                </Box>
+                <Box mt={4}>
+                  <Typography variant='h6'>
+                    Coordinator:&nbsp;
+                    <Link href={`/profile/${challenge?.coordinator}`}>
+                      {challenge?.coordinator}
+                    </Link>
+                  </Typography>
+                </Box>
+                {status === STATUS_ENDED && <>Success Rate: 0</>}
+                <Box mt={4} width={400} maxWidth='100%'>
+                  <Formik
+                    initialValues={{ betAmount }}
+                    onSubmit={handleAgree}
+                    validationSchema={validationSchema}
+                    enableReinitialize
+                  >
+                    {({ values, touched, errors, handleChange }) => {
+                      const handleChangeAndSetBetAmount = (e) => {
+                        handleChange(e)
+                        setBetAmount(e.target.value)
+                      }
+                      return (
+                        <Form autoComplete='off'>
+                          <Grid
+                            container
+                            justifyContent='center'
+                            display='flex'
+                            spacing={2}
+                          >
+                            {status !== STATUS_ENDED &&
+                            localStorage.getItem('identity') !==
+                              challenge?.coordinator &&
+                            !challenge?.fund_delivered_flag ? (
+                              <>
+                                <Grid item xs={12} sm={9}>
+                                  <Field name='minBet' type='number'>
+                                    {({ field }) => (
+                                      <>
+                                        <TextField
+                                          {...field}
+                                          type='number'
+                                          label='Add Bet'
+                                          variant='outlined'
+                                          onChange={handleChangeAndSetBetAmount}
+                                          value={betAmount}
+                                          error={
+                                            touched.minBet &&
+                                            Boolean(errors.minBet)
+                                          }
+                                          helperText={
+                                            touched.minBet && errors.minBet
+                                          }
+                                        />
+                                        {values.betAmount <
+                                          challenge?.add_bet && (
+                                          <p style={{ color: 'red' }}>
+                                            {`Minimum bet amount must be greater than
                                           or equal to ${challenge?.add_bet}`}
-                                            </p>
-                                          )}
-                                        </div>
-                                      )}
-                                    </Field>
-                                    <Box mb={3} />
-                                  </Grid>
+                                          </p>
+                                        )}
+                                      </>
+                                    )}
+                                  </Field>
+                                  <Box mb={3} />
+                                </Grid>
+                                {sending ? (
+                                  <CircularProgress />
+                                ) : (
                                   <Button
                                     variant='contained'
                                     color='primary'
@@ -517,45 +518,46 @@ export default function ChallengeDetail() {
                                   >
                                     Join
                                   </Button>
-                                </>
-                              ) : localStorage.getItem('identity') ===
-                                  challenge?.coordinator &&
-                                !challenge?.fund_delivered_flag ? (
-                                <>
-                                  <Grid item xs={12} sm={9}>
-                                    <Field name='minBet' type='number'>
-                                      {({ field }) => (
-                                        <div>
-                                          <TextField
-                                            {...field}
-                                            type='number'
-                                            label='Add Bet'
-                                            variant='outlined'
-                                            onChange={
-                                              handleChangeAndSetBetAmount
-                                            }
-                                            value={betAmount}
-                                            error={
-                                              touched.minBet &&
-                                              Boolean(errors.minBet)
-                                            }
-                                            helperText={
-                                              touched.minBet && errors.minBet
-                                            }
-                                          />
-                                          {values.betAmount <
-                                            challenge?.add_bet && (
-                                            <p style={{ color: 'red' }}>
-                                              {`Minimum bet amount must be greater than
+                                )}
+                              </>
+                            ) : localStorage.getItem('identity') ===
+                                challenge?.coordinator &&
+                              !challenge?.fund_delivered_flag ? (
+                              <>
+                                <Grid item xs={12} sm={9}>
+                                  <Field name='minBet' type='number'>
+                                    {({ field }) => (
+                                      <div>
+                                        <TextField
+                                          {...field}
+                                          type='number'
+                                          label='Add Bet'
+                                          variant='outlined'
+                                          onChange={handleChangeAndSetBetAmount}
+                                          value={betAmount}
+                                          error={
+                                            touched.minBet &&
+                                            Boolean(errors.minBet)
+                                          }
+                                          helperText={
+                                            touched.minBet && errors.minBet
+                                          }
+                                        />
+                                        {values.betAmount <
+                                          challenge?.add_bet && (
+                                          <p style={{ color: 'red' }}>
+                                            {`Minimum bet amount must be greater than
                                           or equal to ${challenge?.add_bet}`}
-                                            </p>
-                                          )}
-                                        </div>
-                                      )}
-                                    </Field>
-                                    <Box mb={3} />
-                                  </Grid>
-
+                                          </p>
+                                        )}
+                                      </div>
+                                    )}
+                                  </Field>
+                                  <Box mb={3} />
+                                </Grid>
+                                {sending ? (
+                                  <CircularProgress />
+                                ) : (
                                   <Button
                                     variant='contained'
                                     color='primary'
@@ -566,20 +568,20 @@ export default function ChallengeDetail() {
                                   >
                                     Send Payouts
                                   </Button>
-                                </>
-                              ) : (
-                                challenge?.fund_delivered_flag && (
-                                  <Typography variant='h6'>Paid</Typography>
-                                )
-                              )}
-                            </Grid>
-                          </Form>
-                        )
-                      }}
-                    </Formik>
-                  </Box>
-                </>
-              )}
+                                )}
+                              </>
+                            ) : (
+                              challenge?.fund_delivered_flag && (
+                                <Typography variant='h6'>Paid</Typography>
+                              )
+                            )}
+                          </Grid>
+                        </Form>
+                      )
+                    }}
+                  </Formik>
+                </Box>
+              </>
             </Box>
           </Box>
         </PageWrapper>
